cd9640b9694b0c112e948e2dd8a9c81fc5ca10db
DESKTOP-JQD9108\User <denistrigub66@gmail.com>
2022-02-23 19:32:50 +0200

    + функция Rand

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -3,20 +3,30 @@
 using namespace std;
 
 template <typename T>
+int Rand(T x)
+{
+	x = rand() % 10;
+	return x;
+}
+
+template <typename T>
 void Foo(T arr[],const T size = 10)
 {
 	for (T i = 0; i < size; i++)
 	{
-		arr[i] = rand() % 10;
+		arr[i] = Rand(arr[i]);
 		cout << endl;
 		cout << arr[i];
 	}
 
 }
 
+
 int main()
 {
-	
+	const int size = 10;
+	int arr[size];
+	Foo(arr, size);
 	
 	return 0;
 }
