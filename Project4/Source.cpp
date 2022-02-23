a78cb7542200d1782ab7b46276338839714e701d
DESKTOP-JQD9108\User <denistrigub66@gmail.com>
2022-02-23 20:32:02 +0200

    разметка годов и месецов

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 template <typename T>
+
 int Rand(T x)
 {
 	x = rand() % 10;
@@ -10,11 +11,12 @@ int Rand(T x)
 }
 
 template <typename T>
-void Foo(T arr[],const T size = 10)
+
+void Foo(T arr[], const T size = 60)
 {
 	for (T i = 0; i < size; i++)
 	{
-		arr[i] = Rand(arr[i]);
+		arr[i] = rand() % 100 - 1000;
 		cout << endl;
 		cout << arr[i];
 	}
@@ -22,11 +24,289 @@ void Foo(T arr[],const T size = 10)
 }
 
 
+
 int main()
 {
-	const int size = 10;
+	const int size = 60;
 	int arr[size];
-	Foo(arr, size);
-	
-	return 0;
+	int year = 5;
+	switch (year)
+	{
+		//1 year
+	case 1:
+	{
+		int month = 12;
+		switch (month)
+		{
+		case 1:
+		{
+
+		}break;
+		case 2:
+		{
+
+		}break;
+		case 3:
+		{
+
+		}break;
+		case 4:
+		{
+
+		}break;
+		case 5:
+		{
+
+		}break;
+		case 6:
+		{
+
+		}break;
+		case 7:
+		{
+
+		}break;
+		case 8:
+		{
+
+		}break;
+		case 9:
+		{
+
+		}break;
+		case 10:
+		{
+
+		}break;
+		case 11:
+		{
+
+		}break;
+		case 12:
+		{
+		}break;
+		}
+	}break;
+	//2 year
+	case 2:
+	{
+		int month = 12;
+		switch (month)
+		{
+		case 1:
+		{
+
+		}break;
+		case 2:
+		{
+
+		}break;
+		case 3:
+		{
+
+		}break;
+		case 4:
+		{
+
+		}break;
+		case 5:
+		{
+
+		}break;
+		case 6:
+		{
+
+		}break;
+		case 7:
+		{
+
+		}break;
+		case 8:
+		{
+
+		}break;
+		case 9:
+		{
+
+		}break;
+		case 10:
+		{
+
+		}break;
+		case 11:
+		{
+
+		}break;
+		case 12:
+		{
+		}break;
+		}
+	}break;
+	//3 year
+	case 3:
+	{
+		int month = 12;
+		switch (month)
+		{
+		case 1:
+		{
+
+		}break;
+		case 2:
+		{
+
+		}break;
+		case 3:
+		{
+
+		}break;
+		case 4:
+		{
+
+		}break;
+		case 5:
+		{
+
+		}break;
+		case 6:
+		{
+
+		}break;
+		case 7:
+		{
+
+		}break;
+		case 8:
+		{
+
+		}break;
+		case 9:
+		{
+
+		}break;
+		case 10:
+		{
+
+		}break;
+		case 11:
+		{
+
+		}break;
+		case 12:
+		{
+		}break;
+		}
+	}break;
+	//4 year
+	case 4:
+	{
+		int month = 12;
+		switch (month)
+		{
+		case 1:
+		{
+
+		}break;
+		case 2:
+		{
+
+		}break;
+		case 3:
+		{
+
+		}break;
+		case 4:
+		{
+
+		}break;
+		case 5:
+		{
+
+		}break;
+		case 6:
+		{
+
+		}break;
+		case 7:
+		{
+
+		}break;
+		case 8:
+		{
+
+		}break;
+		case 9:
+		{
+
+		}break;
+		case 10:
+		{
+
+		}break;
+		case 11:
+		{
+
+		}break;
+		case 12:
+		{
+		}break;
+		}
+	}break;
+	//5 year
+	case 5:
+	{
+		int month = 12;
+		switch (month)
+		{
+		case 1:
+		{
+
+		}break;
+		case 2:
+		{
+
+		}break;
+		case 3:
+		{
+
+		}break;
+		case 4:
+		{
+
+		}break;
+		case 5:
+		{
+
+		}break;
+		case 6:
+		{
+
+		}break;
+		case 7:
+		{
+
+		}break;
+		case 8:
+		{
+
+		}break;
+		case 9:
+		{
+
+		}break;
+		case 10:
+		{
+
+		}break;
+		case 11:
+		{
+
+		}break;
+		case 12:
+		{
+		}break;
+		}
+	}break;
+	}
+
 }
