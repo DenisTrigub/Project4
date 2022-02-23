5627b04fef73cb0f44b59db303e2137f0b1a6d04
DESKTOP-JQD9108\User <denistrigub66@gmail.com>
2022-02-23 19:17:38 +0200

    Foo done

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
+#include<time.h>
+using namespace std;
 
-int Foo()
+
+void Foo(int arr[],const int size = 10)
 {
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 10;
+		cout << endl;
+		cout << arr[i];
+	}
 
 }
 
 int main()
 {
+	
+	
 	return 0;
 }
