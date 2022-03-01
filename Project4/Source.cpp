675445aa8da72eeebbc1b11ad36e1baa5b063b68
DESKTOP-JQD9108\User <denistrigub66@gmail.com>
2022-03-01 12:07:56 +0200

    done

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -13,89 +13,118 @@ int Rand(T x)
 
 template <typename T>
 
-void Foo(T arr[], const T size = 60)
+void Foo(T arr[], const T size = 30)
 {
+	int sum = 0;
 	for (T i = 0; i < size; i++)
 	{
 		arr[i] = Rand(arr[i]);
 		cout << endl;
 		cout << arr[i];
+		sum += arr[i];
 	}
+	cout << endl;
+	cout << endl;
+	cout << "сума за мес€ц";
+	cout << endl;
+	cout << endl;
+	cout << sum;
+}
 
+template <typename T>
+void Foo2(T arr[], const T size = 1800)
+{
+	int sum = 0;
+	for (T i = 0; i < size; i++)
+	{
+		arr[i] = Rand(arr[i]);
+		cout << endl;
+		cout << arr[i];
+		sum += arr[i];
+	}
+	cout << endl;
+	cout << endl;
+	cout << "сума за год";
+	cout << endl;
+	cout << endl;
+	cout << sum;
 }
 
 
 
 int main()
 {
-	const int size = 60;
+	setlocale(0, "");
+	const int size = 30;
+	const int size2 = 1800;
 	int arr[size];
-	int year = 5;
+	int arr2[size2];
+	int year = 6;
+	cout << " введите год от 1 до 5 \n или введите 6 дл€ того чтоб узнатьб прибыль за 5 лет:";
+	cout << endl;
 	cin >> year;
 	cout << endl;
-    //ввод года
+	//ввод года
 	switch (year)
 	{
-		
+
 		//1 year
 	case 1:
 	{
 		// ввод месеца
 		int month = 12;
 		cin >> month;
+		cout << "введите мес€ц:";
 		switch (month)
 		{
 
 		case 1:
 		{
-			i
-			
-			Foo(arr,size);
-		
-		    
+			Foo(arr, size);
 		}break;
 		case 2:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 3:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 4:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 5:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 6:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 7:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 8:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 9:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 10:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 11:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 12:
 		{
+			Foo(arr, size);
 		}break;
 		}
 	}break;
@@ -103,54 +132,61 @@ int main()
 	case 2:
 	{
 		int month = 12;
+		cout << "введите мес€ц:";
+		cin >> month;
 		switch (month)
 		{
 		case 1:
 		{
 
+
+			Foo(arr, size);
+
+
 		}break;
 		case 2:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 3:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 4:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 5:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 6:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 7:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 8:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 9:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 10:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 11:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 12:
 		{
+			Foo(arr, size);
 		}break;
 		}
 	}break;
@@ -158,54 +194,62 @@ int main()
 	case 3:
 	{
 		int month = 12;
+		
+		cout << "введите мес€ц:";
+		cin >> month;
 		switch (month)
 		{
 		case 1:
 		{
 
+
+			Foo(arr, size);
+
+
 		}break;
 		case 2:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 3:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 4:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 5:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 6:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 7:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 8:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 9:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 10:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 11:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 12:
 		{
+			Foo(arr, size);
 		}break;
 		}
 	}break;
@@ -213,54 +257,61 @@ int main()
 	case 4:
 	{
 		int month = 12;
+		cout << "введите мес€ц:";
+		cin >> month ;
 		switch (month)
 		{
 		case 1:
 		{
 
+
+			Foo(arr, size);
+
+
 		}break;
 		case 2:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 3:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 4:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 5:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 6:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 7:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 8:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 9:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 10:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 11:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 12:
 		{
+			Foo(arr, size);
 		}break;
 		}
 	}break;
@@ -268,57 +319,67 @@ int main()
 	case 5:
 	{
 		int month = 12;
+		cout << "введите мес€ц:";
+		cin >> month;
 		switch (month)
 		{
 		case 1:
 		{
 
+
+			Foo(arr, size);
+
+
 		}break;
 		case 2:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 3:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 4:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 5:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 6:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 7:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 8:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 9:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 10:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 11:
 		{
-
+			Foo(arr, size);
 		}break;
 		case 12:
 		{
+			Foo(arr, size);
 		}break;
 		}
 	}break;
+	case 6:
+	{
+		Foo2(arr2,size2);
+	}break;
 	}
-
 }
